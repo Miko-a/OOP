76703bdfb1ce75d11a7fe27d4f1d90f690c5e1fe
Farrel Jatmiko Aji <farreljatmikoaji@gmail.com>
2025-11-07 16:47:39 +0700

    Update Kendaraan.java

--- RentalKendaraan/Kendaraan.java
+++ RentalKendaraan/Kendaraan.java
@@ -4,7 +4,6 @@ public class Kendaraan {
     private String merk;
     private String model;
     private int tahunProduksi;
-    private boolean tersedia = true;
 
     public Kendaraan(String merk, String model, int tahunProduksi) {
         this.merk = merk;
@@ -12,12 +11,9 @@ public class Kendaraan {
         this.tahunProduksi = tahunProduksi;
     }
 
-    public void setTersedia () { tersedia = false;}
-
     public String getMerk() { return merk;}
     public String getModel() { return model;}
     public int getTahunProduksi() { return tahunProduksi;}
-    public boolean getTersedia() { return tersedia;}
 
     public String detailInformation() {
          return merk + " " + model + " (" + tahunProduksi + ")";
